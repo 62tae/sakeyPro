ba62d8eb245bf4141c6e4dfe67f0a12572d281cf
choiwukseong <choiwukseong@naver.com>
2016-04-19 16:42:07 +0900

    설명 추가

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var express = require('express');
 
 var app = express();
 
+//express 정적파일 사
 app.use(express.static(__dirname + '/public'));
+//engine 사용
 app.set('views',__dirname +'/views');
 app.set('view engine','ejs');
-
+//HomePage 부분
 app.get('/', function(req,res){
   res.render('join');
 });
