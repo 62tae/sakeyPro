aac882d801ade061b8e562105a0325d240868204
choiwukseong <choiwukseong@naver.com>
2016-04-19 16:37:49 +0900

    ejs 모듈 추가 views 폴더 안에 join.ejs 파일 생성 --> engine 사용

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 //모듈 사용
 var express = require('express');
-var mongojs = require('mongojs');
-
-//몽고 디비
-var db = mongojs('sakey',['member']);
 
 var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.set('views',__dirname +'/views');
+app.set('view engine','ejs');
+
+app.get('/', function(req,res){
+  res.render('join');
+});
 
 app.listen(3000, function(){
     console.log('Server Running Connected :3000');
