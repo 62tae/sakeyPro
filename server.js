e7978b463e7e82f05f8928b687b593d168fc7ad5
choiwukseong <choiwukseong@naver.com>
2016-04-20 14:25:32 +0900

    Package.json body-parser 추가

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 //모듈 사용
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
 
 //express 정적파일 사
 app.use(express.static(__dirname + '/public'));
+//bodyParser 사용
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 //engine 사용
 app.set('views',__dirname +'/views');
 app.set('view engine','ejs');
