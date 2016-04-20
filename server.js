6089d66cf59a3f92478fcc79991af96f941999f4
choiwukseong <choiwukseong@naver.com>
2016-04-20 16:53:25 +0900

    server.js 수정

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-//모듈 사용
+require('rootpath')();
 var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
 
-//express 정적파일 사
-app.use(express.static(__dirname + '/public'));
-//bodyParser 사용
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-//engine 사용
-app.set('views',__dirname +'/views');
+var app = express();
+//express 정적파일
 app.set('view engine','ejs');
-//HomePage 부분
+app.set('views', __dirname + '/views');
+
 app.get('/', function(req,res){
   res.render('login');
 });
-// 서버 Port 설정
+
 var server = app.listen(3000, function(){
-    console.log('Server Running Connected :3000');
+  console.log('Server listening at http://');
 });
