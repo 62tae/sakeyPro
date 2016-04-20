69f9ec14656982b1c56b3872aa63cdcbc2e11e53
Ki-Tae You <youkitae62@gmail.com>
2016-04-20 19:43:07 +0900

    Angular App 폴더/파일 추가

--- app/app.js
+++ app/app.js
@@ -24,4 +24,23 @@
 				data: { activeTab: 'account' }
 			});
 	}
-})
+	
+	function run($http, $rootScope, $window) {
+		//add JWT token as default auth header
+		$http.defaults.headers.common['Authorization'] = 'Bearer' + $window.jwToken;
+		
+		//update active tab on state change
+		$rootScope.$on('$stateChangeSuccess', function (event, toState, toPrams, fromState, fromParams) {
+			$rootScope.activeTab = toState.data.activeTab;
+		});
+	}
+	
+	//manually bootstrap angular the JWT token is retrieved from the server
+	$(function(){
+		//get JWT token from server
+		$.get('/app/token', function(token){
+			window.jwtToken = token;
+			angular.bootstrap(document, ['app']);
+		});
+	});
+});
